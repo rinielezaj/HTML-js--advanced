5ff9b64a6d22a606c96036c398f29336be94e36d
rinielezaj <rinielezaj08@gmail.com>
2025-05-24 14:48:35 +0200

    lesson 28

---
+++ lesson 28/Eventet/js/main.js
@@ -0,0 +1,27 @@
+var butoni1 = document.getElementById('btn1')
+var butoni2 = document.getElementById('btn2')
+var butoni3 = document.getElementById('btn3')
+var butoni4 = document.getElementById('btn4')
+
+
+butoni1.onclick = function ()
+{
+ alert("ju sapo keni klikuar butonin")
+}
+
+
+
+butoni2.onmouseover = function ()
+{
+ alert("ju sapo keni klikuar butonin e dyte")
+}
+
+butoni3.onmouseleave = function ()
+{
+ alert("ju sapo keni klikuar butonin e trete")
+}
+
+butoni4.onmouseup = function ()
+{
+ alert("ju sapo keni klikuar butonin e dyte")
+}
