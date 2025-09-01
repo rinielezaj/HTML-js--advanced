bba763fd865a46b64e07780995505452bcbf1b46
rinielezaj <rinielezaj08@gmail.com>
2025-09-01 19:16:56 +0200

    perseritje

---
+++ perseritje/js/main.js
@@ -0,0 +1,39 @@
+var a = 10;
+var b = 12;
+var rez = a + b;
+console.log(rez);
+
+var x = 13;
+var z = 17;
+var rez1 = x * z;
+console.log(rez1);
+
+var n = 99;
+n++;
+console.log(n);
+
+var o = 101;
+o--;
+console.log(o);
+
+var v = 10;
+var w = 10;
+console.log( v == w);
+
+var t = 15;
+var u = 15;
+console.log(t != u);
+
+function showMessage() {
+
+	alert("Test");
+
+}
+
+showMessage();
+
+function sum(num1,num2){
+	return num1 + num2;
+}
+
+console.log(sum(25,25));
